b738c5498144eb2caa26f8ac8be8fbe775cfc3a2
NDChuong <nguyendinhchuong2607@gmail.com>
2018-12-03 01:35:10 +0700

    add redux package

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import rootReducer from './rootReducer'
+
+const middleware = applyMiddleware(thunk, logger);
+
+const store = createStore(rootReducer, middleware);
+
+export default store;
