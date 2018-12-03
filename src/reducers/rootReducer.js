670c2933b2add486058e8e96210a0f182ec91229
Dieu Ngoc Bao <dieungocbao@gmail.com>
2018-12-03 11:08:44 +0700

    add redux folder

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+  
+}
+const rootReducer = (state = initState, action) => {
+    switch (action.type) {
+        case '':
+            break
+        default:
+            break;
+    }
+    return state;
+}
+
+export default rootReducer
